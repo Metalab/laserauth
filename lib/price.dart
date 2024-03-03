d442e0e321bbb1d39f40cf2420f9d2ffa06cb743
Andreas Monitzer <andreas@monitzer.com>
2024-03-03 20:28:36 +0100

    There are 60000 milliseconds in a minute.

--- lib/price.dart
+++ lib/price.dart
@@ -1,6 +1,8 @@
 import 'package:laserauth/cubit/configuration_state.dart';
 
 int centsForLaserTime(Duration duration, {required bool extern, required Configuration configuration}) {
-  return (duration.inMilliseconds * (extern ? configuration.externPricePerMinute : configuration.pricePerMinute) / 6000)
+  return (duration.inMilliseconds *
+          (extern ? configuration.externPricePerMinute : configuration.pricePerMinute) /
+          60000)
       .round();
 }
